d17bb3595302a47dfb33327323a034d7fd713077
Alexey Shmelev <alexsey_89@bk.ru>
2022-01-29 12:12:34 +0300

    frontend js

--- frontend/js/import/bootsrap.js
+++ frontend/js/import/bootsrap.js
@@ -1,29 +1,15 @@
-import Toast from '../../../node_modules/bootstrap/js/src/toast'
+import Alert from '../../../node_modules/bootstrap/js/dist/alert'
+import BaseComponent from '../../../node_modules/bootstrap/js/dist/base-component'
+import Button from '../../../node_modules/bootstrap/js/dist/button'
+import Carousel from '../../../node_modules/bootstrap/js/dist/carousel'
+import Collapse from '../../../node_modules/bootstrap/js/dist/collapse'
+import Dropdown from '../../../node_modules/bootstrap/js/dist/dropdown'
+import Modal from '../../../node_modules/bootstrap/js/dist/modal'
+import Offcanvas from '../../../node_modules/bootstrap/js/dist/offcanvas'
+import Popover from '../../../node_modules/bootstrap/js/dist/popover'
+import ScrollSpy from '../../../node_modules/bootstrap/js/dist/scrollspy'
+import Tab from '../../../node_modules/bootstrap/js/dist/tab'
+import Toast from '../../../node_modules/bootstrap/js/dist/toast'
+import Tooltip from '../../../node_modules/bootstrap/js/dist/tooltip'
 
-/*import Alert from '../../../node_modules/bootstrap/js/src/alert'
-import Button from '../../../node_modules/bootstrap/js/src/button'
-import Carousel from '../../../node_modules/bootstrap/js/src/carousel'
-import Collapse from '../../../node_modules/bootstrap/js/src/collapse'
-import Dropdown from '../../../node_modules/bootstrap/js/src/dropdown'
-import Modal from '../../../node_modules/bootstrap/js/src/modal'
-import Offcanvas from '../../../node_modules/bootstrap/js/src/offcanvas'
-import Popover from '../../../node_modules/bootstrap/js/src/popover'
-import ScrollSpy from '../../../node_modules/bootstrap/js/src/scrollspy'
-import Tab from '../../../node_modules/bootstrap/js/src/tab'
-import Toast from '../../../node_modules/bootstrap/js/src/toast'
-import Tooltip from '../../../node_modules/bootstrap/js/src/tooltip'*/
 
-/*export default {
-  Alert,
-  Button,
-  Carousel,
-  Collapse,
-  Dropdown,
-  Modal,
-  Offcanvas,
-  Popover,
-  ScrollSpy,
-  Tab,
-  Toast,
-  Tooltip
-}*/
